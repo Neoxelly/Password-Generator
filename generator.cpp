3c49eba083f68c20f09c1229d90eaad624c88678
Danin Sadžak <daninsadzak@gmail.com>
2022-10-25 13:38:08 +0200

    Randomized set of upper and lowercase letters

---
+++ generator.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <ctime>
+using namespace std;
+/*
+    Password needs to be 14 characters long
+    Needs to have uppercase and lowercase letters
+    Needs to numbers
+    And one symbol
+*/
+void passwordGenerator(){
+    srand(time(NULL));
+    char lowercase[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    char uppercase[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    char rString1[12];
+    char rString2[12];
+    int number = rand() % 2 + 1;
+    string numberForPassword = to_string(number);
+
+    char symbols[] = {'!', '~', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{', '[', '}', ']', '|', ':', ';', '<', '>', '.', '?', '/'};
+    char symbolForPassword;
+    int i(0);
+    while(i<12){
+        int temp1 = rand() % 26;
+        int temp2 = rand() % 26;
+        rString1[i] = lowercase[temp1];
+        rString2[i] = uppercase[temp2];
+        i++;
+    }
+
+    string password = "";
+    for(int i = 1; i <= 12; i++){
+        password.append(string(&rString1[i], 1));
+        password.append(string(&rString2[i], 1));
+    }
+    cout<<password;
+}
+int main(){
+    cout<<"Generated Password:\n";
+    passwordGenerator();
+    return 0;
+}
