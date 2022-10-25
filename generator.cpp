5ba4cd8989b9767654d38b81bfd6eaf606634a53
Danin Sadžak <daninsadzak@gmail.com>
2022-10-25 21:33:25 +0200

    All done!

--- generator.cpp
+++ generator.cpp
@@ -6,28 +6,25 @@ using namespace std;
 /*
     Password needs to be 14 characters long
     Needs to have uppercase and lowercase letters
-    Needs to numbers
+    Needs to have one number
     And one symbol
 */
 void passwordGenerator(){
     srand(time(NULL));
     char lowercase[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
     char uppercase[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-    char rString1[12];
-    char rString2[12];
+    char rString1[13];
+    char rString2[13];
     int number = rand() % 99 + 1;
-
-    //char symbols[] = {'!', '~', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{', '[', '}', ']', '|', ':', ';', '<', '>', '.', '?', '/'};
-
     int i(0);
-    while(i<12){
+    while(i < 13){
         int temp1 = rand() % 26;
         int temp2 = rand() % 26;
         rString1[i] = lowercase[temp1];
         rString2[i] = uppercase[temp2];
         i++;
     }
-
+    int br(0);
     string password = "";
     for(int i = 1; i <= 12; i++){
         password.append(string(&rString1[i], 1));
@@ -37,7 +34,16 @@ void passwordGenerator(){
     cout<<number;
 }
 int main(){
-    cout<<"Generated Password:\n";
+    again:cout<<"Generated Password:\n";
     passwordGenerator();
+    cout<<endl;
+    cout<<"Do you need to generate a new one? (Y/N)\n";
+    char answer;
+    input:cin>>answer;
+    switch(answer){
+        case 'Y': case 'y': goto again; break;
+        case 'N': case 'n': cout<<"See you next time!"; break;
+        default: cout<<"Unknown input (Y/N):\n"; goto input; break;
+    }
     return 0;
 }
